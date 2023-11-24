6b8bc5184d03c81c4f056d232286df03b92c382a
Tanmaypatil123 <tanmaypatil3151@gmail.com>
2023-11-24 22:12:24 +0530

    modified secrets

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ dotenv.load_dotenv()
 class Config :
     # open_ai_key = os.getenv("OPENAI_API_KEY")
     # assembly_ai_key = os.getenv("ASSEMBLY_AI_API_KEY")
-    open_ai_key = st.secrets("open_ai_key")
-    assembly_ai_key = st.secrets("assembly_ai_key")
+    open_ai_key = st.secrets["open_ai_key"]
+    assembly_ai_key = st.secrets["assembly_ai_key"]
     
